9def2f34f5caa71c2fb3cc77f5c1a6ca2ae9fdf7
erikmackie <erik.mackie@bristol.ac.uk>
2016-01-08 10:42:05 +0000

    Tweepy_test now streaming #alcoholguidelines

--- tweepy_test.py
+++ tweepy_test.py
@@ -11,6 +11,23 @@ auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
 
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status.text) 
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+ 
+class MyListener(StreamListener):
+ 
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#alcoholguidelines'])
