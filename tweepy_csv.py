20d12d6ec3f22ceff8b04a4d39c90fdaa426cebe
Hintzen <reh12@ic.ac.uk>
2016-01-08 12:16:43 +0000

    new search term

--- tweepy_csv.py
+++ tweepy_csv.py
@@ -1,21 +1,21 @@
 import tweepy
 import csv
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
+consumer_key = 'hxJALVpXO0DHOB8rqfGQrqo5d'
+consumer_secret = 'zN4XToW9NHuhEQlC13X7mjMIpdf74UDRdxySolCKqlwZyrL4Kj'
+access_token = '2925200369-Ih0UU53AuEALRXMBQhtqOjJmNJeF23e63qHRLVI'
+access_secret = '3rIgo5eZ9NhLUVTL7UZG36B1LAdc5TUcrchVAVAFd8feF'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 # Open/Create a file to append data
-csvFile = open('tweets.csv', 'a')
+csvFile = open('friendtweets.csv', 'a')
 #Use csv Writer
 csvWriter = csv.writer(csvFile)
 
-for tweet in tweepy.Cursor(api.search,q="#alcoholguidelines").items():
+for tweet in tweepy.Cursor(api.search,q="#LoseAFriendIn3Words").items():
 	if tweet.coordinates != None:
 		print(tweet.created_at, tweet.text, tweet.coordinates)
 		csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8'), tweet.coordinates])
