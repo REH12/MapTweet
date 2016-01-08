5680587712633543a91f17a88190bc15ab89444f
Adriana <adriana@MattAdrianas-MacBook-Pro.local>
2016-01-08 14:59:21 +0000

    More generic - just chnage the input file geotweets.txt

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,20 +1,20 @@
 
 # coding: utf-8
 
-# In[122]:
+# In[263]:
 
 import csv
 import numpy
 
 
-# In[67]:
+# In[264]:
 
-exampleFile = open('tweets.csv')
+exampleFile = open('geotweets.csv')
 exampleReader = csv.reader(exampleFile)
 exampleData = list(exampleReader)
 
 
-# In[178]:
+# In[265]:
 
 #I'd rather do this:
 # from FormatTweet import stripTweet
@@ -45,18 +45,18 @@ def stripTweet(ortweet):
     return(twords_min)
 
 
-# In[197]:
+# In[266]:
 
 eachone = []
 i = 0
-for r in range(0,10,2):
+for r in range(0,len(exampleData),):
     current = stripTweet(exampleData[r][1])
     del current [0]
     eachone.append(current)
     i+=1
 
 
-# In[190]:
+# In[267]:
 
 #Now use the classifier
 import nltk.classify.util
@@ -64,41 +64,41 @@ from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
 
 
-# In[191]:
+# In[268]:
 
 def word_feats(words):
     return dict([(word, True) for word in words])
 
 
-# In[182]:
+# In[269]:
 
 nltk.download('movie_reviews')
 
 
-# In[183]:
+# In[270]:
 
 negids = movie_reviews.fileids('neg')
 posids = movie_reviews.fileids('pos')
 
 
-# In[184]:
+# In[271]:
 
 negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
 posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
 
 
-# In[185]:
+# In[272]:
 
 trainfeats = negfeats[:750] + posfeats[:750]
 testfeats = negfeats[751:] + posfeats[751:]
 
 
-# In[186]:
+# In[273]:
 
 classifier = NaiveBayesClassifier.train(trainfeats)
 
 
-# In[194]:
+# In[274]:
 
 results = []
 
@@ -107,9 +107,40 @@ for c in eachone:
     results.append(res)
 
 
-# In[198]:
+# In[278]:
 
-print(results)
+counts = Counter(results)
+
+
+# In[276]:
+
+print(counts)
+
+
+# In[305]:
+
+values = list(counts.values())
+
+
+# In[277]:
+
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+
+
+# In[307]:
+
+# Data to plot
+labels = 'Negative', 'Positive'
+sizes = [values[0]/sum(values), values[1]/sum(values)]
+colors = ['lightcoral', 'lightskyblue']
+ 
+# Plot
+plt.pie(sizes, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=False, startangle=140)
+ 
+plt.axis('equal')
+plt.show()
 
 
 # In[ ]:
