ad4875197bc075fab1965d0677bd70b626fbccdd
erikmackie <erik.mackie@bristol.ac.uk>
2016-01-08 10:20:58 +0000

    Tweepy Test adapted

--- tweepy_test.py
+++ tweepy_test.py
@@ -1,10 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
  
-consumer_key = 'ZNFGmPbvdIYgphOShBqnDNz88'
-consumer_secret = 'GxVdMqRDmnL9BpAmHdtxI9O1LF08x4Vx8juQ62XTqbCiYqs0Vi'
-access_token = '2801629397-XX7B1tHKTaJ0JDBkybCF9c6x9Nh4F7kjIswyHED'
-access_secret = 'ZzVBDbHq3P958dffh36bj17Frjhch9HfyTT3VyPzZQRfI'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
  
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
